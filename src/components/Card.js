6bd22cb640bd61ff37d9c72ab16dd99933f8ec16
Edivan Silva <codedavinci.now@gmail.com>
2018-10-26 16:01:39 -0400

    feat(Card): Add Card Component

---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Card = ({ children }) => {
+  return <div className="card">{children}</div>;
+};
+
+Card.propTypes = {};
+
+export default Card;
